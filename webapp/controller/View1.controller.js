c3b63aacea9e5e22a25008b281b05e2cb7781352
DiegoSN1 <diegodns1@gmail.com>
2024-12-04 09:01:52 -0300

    Ajuste view e controler

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -11,6 +11,18 @@ function (Controller) {
         onPress: function(){
             alert("hello world");
   
-          }
+          },
+      onCalcular: function(){
+        var oView = this.getView();
+        var iB1 = parseInt(oView.byId("b1").getValue());
+        var iB2 = parseInt(oView.byId("b2").getValue());
+        var iB3 = parseInt(oView.byId("b3").getValue());
+        var iB4 = parseInt(oView.byId("b4").getValue());
+        var fResultado = 0;
+
+        fResultado = ( iB1 + iB2 + iB3 + iB4 );
+
+        oView.byId("resultado").setValue(fResultado);
+      }
     });
 });
